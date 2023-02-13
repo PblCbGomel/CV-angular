354c9dbb5bda4125114ac167ecdfe6d629802710
PblCbGomel <pblcb.gomel@gmail.com>
2023-02-13 19:31:06 +0300

    feat: add primeflex and navigation in header

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { PanelModule } from 'primeng/panel';
 import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
+import { TabMenuModule } from 'primeng/tabmenu';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, PanelModule, ButtonModule, MenuModule],
-  exports: [CommonModule, PanelModule, ButtonModule, MenuModule],
+  imports: [CommonModule, PanelModule, ButtonModule, MenuModule, TabMenuModule],
+  exports: [PanelModule, ButtonModule, MenuModule, TabMenuModule],
 })
 export class PrimeNGModule {}
--- src/app/share/components/header/navigation/navigation.component.ts
+++ src/app/share/components/header/navigation/navigation.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.scss']
+  styleUrls: ['./navigation.component.scss'],
 })
 export class NavigationComponent {
+  items: MenuItem[];
+  styles: object;
 
+  ngOnInit() {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-fw pi-home', routerLink: 'main' },
+      { label: 'About me', icon: 'pi pi-fw pi-user', routerLink: 'about' },
+      { label: 'Skills', icon: 'pi pi-fw pi-sitemap', routerLink: 'skills' },
+      { label: 'Projects', icon: 'pi pi-fw pi-folder', routerLink: 'projects' },
+      {
+        label: 'Contacts',
+        icon: 'pi pi-fw pi-comments',
+        routerLink: 'contacts',
+      },
+    ];
+  }
 }
