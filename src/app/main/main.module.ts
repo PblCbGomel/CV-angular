7ffd2d955e2a71b117126d0ef98594e189a6fbd2
PblCbGomel <pblcb.gomel@gmail.com>
2023-02-15 01:39:10 +0300

    feat: add 2 component templates

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -7,10 +7,17 @@ import { AboutComponent } from './components/about/about.component';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ContactsComponent } from './components/contacts/contacts.component';
+import { PrimeNGModule } from '../prime-ng/prime-ng.module';
 
 @NgModule({
-  declarations: [MainComponent, AboutComponent, SkillsComponent, ProjectsComponent, ContactsComponent],
-  imports: [CommonModule, MainRoutingModule],
+  declarations: [
+    MainComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactsComponent,
+  ],
+  imports: [CommonModule, MainRoutingModule, PrimeNGModule],
   exports: [MainComponent],
 })
 export class MainModule {}
--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -4,10 +4,18 @@ import { PanelModule } from 'primeng/panel';
 import { ButtonModule } from 'primeng/button';
 import { MenuModule } from 'primeng/menu';
 import { TabMenuModule } from 'primeng/tabmenu';
+import { ImageModule } from 'primeng/image';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, PanelModule, ButtonModule, MenuModule, TabMenuModule],
-  exports: [PanelModule, ButtonModule, MenuModule, TabMenuModule],
+  imports: [
+    CommonModule,
+    PanelModule,
+    ButtonModule,
+    MenuModule,
+    TabMenuModule,
+    ImageModule,
+  ],
+  exports: [PanelModule, ButtonModule, MenuModule, TabMenuModule, ImageModule],
 })
 export class PrimeNGModule {}
